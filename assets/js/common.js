dc74c3c007e223c5fa8fc133282b3d6331b5299b
Zhenquan.Liang <liangzq@edaotech.com>
2017-09-28 14:35:01 +0800

    + add national/festival day banner

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,3 +3,19 @@ function TabSwitch(theClass){
     $(".switch_view").toggleClass("current_view")
     $(".switchBtn").toggleClass("btn_account_bg")
 }
+
+/*job*/
+$(function(){
+    jQuery.noConflict();
+    $('.careers-title').toggle(
+        function(){
+            $(this).addClass('active');
+            $(this).parent().find('.careers-content').slideToggle();
+        },
+        function(){
+            $(this).removeClass('active');
+            $(this).parent().find('.careers-content').slideToggle();
+        }
+    );
+    $('.careers-title').eq(0).click();
+});
